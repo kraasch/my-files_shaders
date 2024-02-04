25f1ec164a8c11e8ebe0d97057499344dea560dc
Alex Kraasch <alex@kraasch.eu>
2024-02-04 04:50:20 +0100

    Update EXPORT script.

--- shaders/01_glsl-viewer/export.sh
+++ shaders/01_glsl-viewer/export.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 mkdir -p ./showcase
+
+function append_readme() {
+	num="$1"
+	ending="$2"
+	beg="<img width='100' alt='demo' src='./examples/"
+	end="'>|"
+	echo "${beg}${num}.${ending}${end}" >> README.md.example
+}
+
 for x in {01..37}; do
-	frame_rate=24
+	frame_rate=5
 	cp "$x"* 00_new.frag
 	rm -rf ./abcxyz
 	mkdir ./abcxyz
@@ -12,10 +21,12 @@ for x in {01..37}; do
 	res=$(diff 00000.png 00001.png)
 	if $(test -z "$res"); then
 		mv 00000.png ../showcase/"$x".png
+		append_readme "$x" png
 	else
 		# NOTE: for mp4 export.
 		# ffmpeg -framerate "$frame_rate" -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p ../showcase/"$x".mp4
 		convert -delay 10 -loop 0 *.png ../showcase/"$x".gif
+		append_readme "$x" gif
 	fi
 	cd ..
 done
