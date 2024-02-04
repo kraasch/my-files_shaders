e2587c3dafbcbabb028d8d508eed99084f583f32
Alex Kraasch <alex@kraasch.eu>
2024-02-04 04:14:11 +0100

    Add fast EXPORT script.

---
+++ shaders/01_glsl-viewer/export.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p ./showcase
+for x in {01..37}; do
+	frame_rate=24
+	cp "$x"* 00_new.frag
+	rm -rf ./abcxyz
+	mkdir ./abcxyz
+	glslViewer -l -x 5 -y 500 -w 180 -h 180 00_new.frag -E "sequence,0,10,$frame_rate"
+	mv *png ./abcxyz
+	cd ./abcxyz
+	res=$(diff 00000.png 00001.png)
+	if $(test -z "$res"); then
+		mv 00000.png ../showcase/"$x".png
+	else
+		ffmpeg -framerate "$frame_rate" -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p ../showcase/"$x".mp4
+	fi
+	cd ..
+done
+rm -rf ./abcxyz
